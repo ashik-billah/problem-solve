b09badfd6a77159f8e38ef4876ec85771b0cfa48
ashik-billah <ashik.210101@s.pust.ac.bd>
2024-07-03 13:59:39 +0600

    second problem

---
+++ 263 A. Beautiful Matrix.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+    int n,x=0;
+    string s;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        if(s[1]=='+')
+        {
+            x++;
+
+        }
+        if(s[1]=='-')
+        {
+            x--;
+        }
+    }
+    cout<<x;
+
+
+    return 0;
+}
